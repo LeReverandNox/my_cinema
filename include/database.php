8e5020f044ab6e596dcebfbe37e4cd730fc13789
laidet_r <rodolphe.laidet@epitech.eu>
2015-11-08 16:53:38 +0100

    Modification des identifiants de connexion

--- include/database.php
+++ include/database.php
@@ -1,6 +1,6 @@
 <?php
         try {
-            $database = new PDO('mysql:host=localhost;dbname=epitech_tp;charset=utf8', 'root', '#W0u1d@Y0u&K1nd1y$', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $database = new PDO('mysql:host=localhost;dbname=epitech_tp;charset=utf8', 'pangolin', 'password');
         }
         catch (Exception $e) {
             Die ('Erreur : ' . $e->getMessage());
