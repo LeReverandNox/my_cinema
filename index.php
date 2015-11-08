0cf6252526b433620d693fb2fadb20eed3f95910
laidet_r <rodolphe.laidet@epitech.eu>
2015-11-08 15:31:26 +0100

    Rajout d'un peu de texte sur l'index

--- index.php
+++ index.php
@@ -9,10 +9,17 @@
 <body>
     <div class="main_wrapper">
         <header>
-            <h1>Hólà amigo !</h1>
+            <h1>My Cinema Manager v1.0</h1>
             <?php include "template/nav.html" ?>
         </header>
             <h2>Bienvenue  !</h2>
+            <div class="center">
+            <p>Pour cause de restriction budgétaire et de plan d'austérité, </p>
+            <p>ceci une une page d'accueil particuliement vide...</p>
+            <p>Veuillez ne pas vous attarder dessus.</p>
+            <p>Bien cordialement</p>
+            <p>Le peintre des pancartes.</p>
+            </div>
     </div>
 </body>
 </html>
